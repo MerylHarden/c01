d16948894d51575e52a1b93564c4222b12b35af0
MerylHarden <merylharden@gmail.com>
2016-05-18 16:09:49 -0700

    added multiple comments to js file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 //this script displays a greeting to the user based upon the current time
 
-var today = new Date();
-var hourNow = today.getHours();
+var today = new Date(); //create a new date object
+var hourNow = today.getHours(); //find the current hour
 var greeting;
+
+//display the appropriate greeting based on the current time
+
+/* this is how you do a multiline comment if you need to
+blah blah blah */
 
 if (hourNow > 18) {
 	greeting = 'Good evening!';
